89bc3b31a09e6dda8600fac8ce35ae8811008e79
tterpi <the.o@live.de>
2020-03-03 23:32:05 +0100

    Complete spline can be meshed.
    ExtrudeTest was expanded to allow meshing an entire spline. The control
    point game objects were used again as the control points for the spline
    and the interpolated points are used to calculate the positions and
    tangents used to transform the cross sections that are then connected
    with triangles.

--- Assets/Scripts/ExtrudeTest.cs
+++ Assets/Scripts/ExtrudeTest.cs
@@ -1,17 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KochanekBartelsSplines;
 
 public class ExtrudeTest : MonoBehaviour
 {
+    public float crossSectionScale = .3f;
+
+    List<Vector3> transformPoints(List<Vector3> points, Vector3 position, Vector3 tangent) {
+
+        Matrix4x4 matrix = Matrix4x4.TRS(position, Quaternion.FromToRotation(Vector3.up, tangent), new Vector3(crossSectionScale, crossSectionScale, crossSectionScale));
+
+        List<Vector3> pointsTransformed = new List<Vector3>();
+
+        foreach (Vector3 point in points)
+        {
+            pointsTransformed.Add(matrix.MultiplyPoint3x4(point));
+        }
+
+        return pointsTransformed;
+    }
+
+    List<Vector3> transformNormals(List<Vector3> normals, Vector3 tangent)
+    {
+
+        Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.FromToRotation(Vector3.up, tangent), Vector3.one);
+
+        List<Vector3> normalsTransformed = new List<Vector3>();
+
+        foreach (Vector3 normal in normals)
+        {
+            normalsTransformed.Add(matrix.MultiplyVector(normal));
+        }
+
+        return normalsTransformed;
+    }
+
+    List<int> generateTriangles(int numOfCrossSectionVertices, int numOfSections) {
+
+        List<int> triangles = new List<int>();
+
+        for (int j = 0; j < numOfSections; j++) {
+            for (int i = j * numOfCrossSectionVertices; i < (j+1) * numOfCrossSectionVertices; i++)
+            {
+                if (i % numOfCrossSectionVertices == numOfCrossSectionVertices - 1)
+                {
+                    triangles.Add(i);
+                    triangles.Add(i + 1);
+                    triangles.Add(i - numOfCrossSectionVertices + 1);
+
+                    triangles.Add(i);
+                    triangles.Add(i + numOfCrossSectionVertices);
+                    triangles.Add(i + 1);
+                }
+                else
+                {
+                    triangles.Add(i);
+                    triangles.Add(i + 1 + numOfCrossSectionVertices);
+                    triangles.Add(i + 1);
+
+                    triangles.Add(i);
+                    triangles.Add(i + numOfCrossSectionVertices);
+                    triangles.Add(i + 1 + numOfCrossSectionVertices);
+                }
+            }
+        }
+
+        return triangles;
+    }
+
+
+    private Vector3[] ControlPoints;
+
+    KochanekBartelsSpline kochanekBartelsSpline;
+
+    [SerializeField]
+    private GameObject[] ControlPointObjects;
+
+    private List<Vector3> InterpolatedPoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
-        //Mesh mesh = meshFilter.sharedMesh;
-        Mesh mesh = new Mesh();
-
-
         List<Vector3> crossSectionShape = new List<Vector3> { new Vector3(1f, 0f, 0.5f), new Vector3(1f, 0f, -0.5f), new Vector3(0f, 0f, -1f), new Vector3(-1f, 0f, -0.5f), new Vector3(-1f, 0f, 0.5f), new Vector3(0f, 0f, 1f) };
         crossSectionShape.Reverse();
         List<Vector3> crossSectionShapeNormals = new List<Vector3>();
@@ -19,61 +89,50 @@ public class ExtrudeTest : MonoBehaviour
             crossSectionShapeNormals.Add(point.normalized);
         }
 
-        //extrude transform
-        Matrix4x4 matrix = Matrix4x4.TRS(new Vector3(0f, 5f, 1f), Quaternion.Euler(45f, 0, 0f), Vector3.one);
-        Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(45f, 0, 0f));
+        kochanekBartelsSpline = new KochanekBartelsSpline();
 
-        List<Vector3> crossSectionTransformed = new List<Vector3>();
-        List<Vector3> normalsTransformed = new List<Vector3>();
-
-
-        foreach (Vector3 point in crossSectionShape) {
-            crossSectionTransformed.Add( matrix.MultiplyPoint3x4(point));
-        }
-
-        foreach (Vector3 normal in crossSectionShapeNormals)
+        ControlPoints = new Vector3[ControlPointObjects.Length];
+        for (int i = 0; i < ControlPointObjects.Length; i++)
         {
-            normalsTransformed.Add(matrix.MultiplyVector(normal));
+            ControlPoints[i] = ControlPointObjects[i].transform.position;
         }
 
-        List<Vector3> vertices = new List<Vector3>(crossSectionShape);
-        vertices.AddRange(crossSectionTransformed);
-        //mesh.SetVertices(vertices);
+        InterpolatedPoints = kochanekBartelsSpline.InterpolatedPoints;
+        kochanekBartelsSpline.setControlPoints(ControlPoints);
 
-        List<Vector3> normals = new List<Vector3>(crossSectionShapeNormals);
-        normals.AddRange(normalsTransformed);
-        //mesh.SetNormals(normals);
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
 
-        List<int> triangles = new List<int>();
+        //first segment
+        Vector3 tangent = (InterpolatedPoints[1] - InterpolatedPoints[0]);
+        List<Vector3> firstCrossSectionVertices = transformPoints(crossSectionShape, InterpolatedPoints[0], tangent);
+        List<Vector3> firstCrossSectionNormals = transformNormals(crossSectionShapeNormals, tangent);
+        vertices.AddRange(firstCrossSectionVertices);
+        normals.AddRange(firstCrossSectionNormals);
 
-        for (int i = 0; i < crossSectionShape.Count; i++) {
-            if (i % crossSectionShape.Count == crossSectionShape.Count - 1)
-            {
-                triangles.Add(i);
-                triangles.Add(i + 1);
-                triangles.Add(i - crossSectionShape.Count + 1);
-
-                triangles.Add(i);
-                triangles.Add(i + crossSectionShape.Count);
-                triangles.Add(i + 1);
-            }
-            else {
-                triangles.Add(i);
-                triangles.Add(i + 1 + crossSectionShape.Count);
-                triangles.Add(i + 1);
-
-                triangles.Add(i);
-                triangles.Add(i + crossSectionShape.Count);
-                triangles.Add(i + 1 + crossSectionShape.Count);
-            }
+        //middle segments
+        for (int i = 1; i < InterpolatedPoints.Count - 1; i++) {
+            tangent = (InterpolatedPoints[i] - InterpolatedPoints[i - 1]) + (InterpolatedPoints[i + 1] - InterpolatedPoints[i]) / 2f;
+            vertices.AddRange(transformPoints(crossSectionShape, InterpolatedPoints[i], tangent));
+            normals.AddRange(transformNormals(crossSectionShapeNormals, tangent));
         }
+
+        //last segment
+        tangent = (InterpolatedPoints[InterpolatedPoints.Count-1] - InterpolatedPoints[InterpolatedPoints.Count-2]);
+        List<Vector3> lastCrossSectionVertices = transformPoints(crossSectionShape, InterpolatedPoints[InterpolatedPoints.Count - 1], tangent);
+        List<Vector3> lastCrossSectionNormals = transformNormals(crossSectionShapeNormals, tangent);
+        vertices.AddRange(lastCrossSectionVertices);
+        normals.AddRange(lastCrossSectionNormals);
+
+        List<int> triangles = generateTriangles(crossSectionShape.Count, InterpolatedPoints.Count-1);
 
         //generate caps
         //begin cap
 
         List<int> beginCapTriangles = new List<int>();
 
-        for (int i = 1; i <= crossSectionShape.Count - 2; i++) {
+        for (int i = 1; i <= crossSectionShape.Count - 2; i++)
+        {
             int firstVertex = vertices.Count;
 
             beginCapTriangles.Add(firstVertex);
@@ -81,10 +140,11 @@ public class ExtrudeTest : MonoBehaviour
             beginCapTriangles.Add(firstVertex + i + 1);
         }
 
-        vertices.AddRange(crossSectionShape);
+        vertices.AddRange(firstCrossSectionVertices);
 
-        Vector3 beginCapNormal = Vector3.Cross(crossSectionShape[1] - crossSectionShape[0], crossSectionShape[2] - crossSectionShape[0]).normalized;
-        for (int i = 0; i < crossSectionShape.Count; i++) {
+        Vector3 beginCapNormal = Vector3.Cross(firstCrossSectionVertices[1] - firstCrossSectionVertices[0], firstCrossSectionVertices[2] - firstCrossSectionVertices[0]).normalized;
+        for (int i = 0; i < crossSectionShape.Count; i++)
+        {
             normals.Add(beginCapNormal);
         }
 
@@ -101,13 +161,17 @@ public class ExtrudeTest : MonoBehaviour
             endCapTriangles.Add(firstVertex + i);
         }
 
-        vertices.AddRange(crossSectionTransformed);
-        Vector3 endCapNormal = -Vector3.Cross(crossSectionTransformed[1] - crossSectionTransformed[0], crossSectionTransformed[2] - crossSectionTransformed[0]).normalized;
-        for (int i = 0; i < crossSectionTransformed.Count; i++)
+        vertices.AddRange(lastCrossSectionVertices);
+        Vector3 endCapNormal = -Vector3.Cross(lastCrossSectionVertices[1] - lastCrossSectionVertices[0], lastCrossSectionVertices[2] - lastCrossSectionVertices[0]).normalized;
+        for (int i = 0; i < lastCrossSectionVertices.Count; i++)
         {
             normals.Add(endCapNormal);
         }
 
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        //Mesh mesh = meshFilter.sharedMesh;
+        Mesh mesh = new Mesh();
 
         mesh.SetVertices(vertices);
         mesh.SetNormals(normals);
@@ -118,11 +182,5 @@ public class ExtrudeTest : MonoBehaviour
 
         //mesh.UploadMeshData(false);
         meshFilter.mesh = mesh;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
