32397847c5b08eb2fccf4f34623908f82d6307f8
tterpi <the.o@live.de>
2020-11-09 13:31:31 +0100

    UndoRedo: Added CommmandInvoker and Command class.

---
+++ Assets/Scripts/Interaction/Commands/Command.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRSketchingGeometry.Commands {
+    public abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void Undo();
+        public abstract void Redo();
+    }
+}
---
+++ Assets/Scripts/Interaction/Commands/CommandInvoker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRSketchingGeometry.Commands{
+    public class CommandInvoker
+    {
+        private Stack<Command> undoStack = new Stack<Command>();
+        private Stack<Command> redoStack = new Stack<Command>();
+
+        public void ExecuteCommand(Command command) {
+            command.Execute();
+            undoStack.Push(command);
+            redoStack.Clear();
+        }
+
+        public void Undo() {
+            if (undoStack.Count <= 0) {
+                return;
+            }
+            Command executedCommand = undoStack.Pop();
+            executedCommand.Undo();
+            redoStack.Push(executedCommand);
+        }
+
+        public void Redo() {
+            if (redoStack.Count <= 0)
+            {
+                return;
+            }
+            Command undoneCommand = redoStack.Pop();
+            undoneCommand.Redo();
+            undoStack.Push(undoneCommand);
+        }
+    }
+}
