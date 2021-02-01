3ef25f85ca65cf87cd9ea5bb06f9bb2c271b4d45
tterpi <the.o@live.de>
2021-02-02 00:54:14 +0100

    Generate a ribbon mesh from points and rotations.

---
+++ Assets/Plugins/VRSketchingGeometry/Scripts/ModelingKernel/RibbonMesh.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VRSketchingGeometry.Meshing
+{
+    class RibbonMesh
+    {
+
+        public static Mesh GetRibbonMesh(List<Vector3> points, List<Quaternion> rotations, float width) {
+
+            List<Vector3> ribbonCrossSection = new List<Vector3> { new Vector3(0, 0, .5f), new Vector3(0, 0, 0), new Vector3(0, 0, -.5f) };
+            return GetMesh(ribbonCrossSection, points, rotations, Vector3.one * width);
+        }
+
+        public static Mesh GetMesh(List<Vector3> crossSection, List<Vector3> points, List<Quaternion> rotations, Vector3 scale) {
+
+            List<Vector3> vertices = new List<Vector3>();
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                List<Vector3> transformedCrossSection = new List<Vector3>();
+                for (int j = 0; j < crossSection.Count; j++)
+                {
+                    transformedCrossSection.Add(TransformPoint(crossSection[j], points[i], rotations[i], scale));
+                }
+
+                vertices.AddRange(transformedCrossSection);
+
+            }
+
+            List<int> triangles = LineExtruder.GenerateTriangles(crossSection.Count, points.Count-1);
+
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(vertices);
+            mesh.SetTriangles(triangles.ToArray(), 0);
+            mesh.RecalculateNormals();
+            mesh.SetUVs(0, TextureCoordinates.GenerateQuadrilateralUVsStretchU(vertices.Count, crossSection.Count));
+            mesh.RecalculateTangents();
+
+            return mesh;
+        }
+
+        public static Vector3 TransformPoint(Vector3 point, Vector3 position, Quaternion rotation, Vector3 scale) {
+            Matrix4x4 matrix = Matrix4x4.TRS(position, rotation, scale);
+            return matrix.MultiplyPoint3x4(point);
+        }
+
+    }
+}
---
+++ Assets/Plugins/VRSketchingGeometry/Scripts/Test/RibbonTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRSketchingGeometry.Meshing;
+
+public class RibbonTest : MonoBehaviour
+{
+    public GameObject ControlPointParent;
+
+    private void generateRibbon() {
+        List<Vector3> points = new List<Vector3>();
+        List<Quaternion> rotations = new List<Quaternion>();
+        foreach (Transform controlPoint in ControlPointParent.transform)
+        {
+            points.Add(controlPoint.position);
+            rotations.Add(controlPoint.rotation);
+        }
+
+        GetComponent<MeshFilter>().mesh = RibbonMesh.GetRibbonMesh(points, rotations, .4f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        generateRibbon();
+    }
+}
