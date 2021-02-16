53f3d414ccd7eca0d742bf7427555cdd335f949a
tterpi <the.o@live.de>
2021-02-16 22:18:36 +0100

    Introduced unit tests for commands that use SketchWorld.
    This uses Unity Test Framework based on NUnit. Created a test scene that
    contains required prefab instances for testing.

---
+++ Assets/Plugins/VRSketchingGeometry/Scripts/Test/UnitTest/SketchWorldCommandTest.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using VRSketchingGeometry;
+using VRSketchingGeometry.Commands;
+using VRSketchingGeometry.SketchObjectManagement;
+
+namespace Tests
+{
+    public class SketchWorldCommandTest
+    {
+        private SketchWorld SketchWorld;
+        private LineSketchObject LineSketchObject;
+        private CommandInvoker Invoker;
+
+        [UnitySetUp]
+        public IEnumerator SetUpScene() {
+            yield return SceneManager.LoadSceneAsync("CommandTestScene", LoadSceneMode.Single);
+            this.SketchWorld = GameObject.FindObjectOfType<SketchWorld>();
+            this.LineSketchObject = GameObject.FindObjectOfType<LineSketchObject>();
+            yield return null;
+            Invoker = new CommandInvoker();
+        }
+
+        [Test]
+        public void AddObjectToSketchWorldTest()
+        {
+            AddObjectToSketchWorldRootCommand addCommand = new AddObjectToSketchWorldRootCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(addCommand);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "RootSketchObjectGroup");
+        }
+
+        [Test]
+        public void AddObjectToSketchWorldUndoTest()
+        {
+            AddObjectToSketchWorldRootCommand addCommand = new AddObjectToSketchWorldRootCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(addCommand);
+            Invoker.Undo();
+            Assert.IsFalse(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "Deleted Bin");
+        }
+
+        [Test]
+        public void AddObjectToSketchWorldRedoTest()
+        {
+            AddObjectToSketchWorldRootCommand addCommand = new AddObjectToSketchWorldRootCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(addCommand);
+            Invoker.Undo();
+            Assert.IsFalse(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "Deleted Bin");
+
+            Invoker.Redo();
+            Assert.IsTrue(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "RootSketchObjectGroup");
+        }
+
+        [Test]
+        public void DeleteObjectTest()
+        {
+            AddObjectToSketchWorldRootCommand addCommand = new AddObjectToSketchWorldRootCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(addCommand);
+            Assert.IsTrue(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "RootSketchObjectGroup");
+
+            DeleteObjectCommand deleteCommand = new DeleteObjectCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(deleteCommand);
+            Assert.IsFalse(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "Deleted Bin");
+        }
+
+        [Test]
+        public void DeleteObjectUndoTest()
+        {
+            AddObjectToSketchWorldRootCommand addCommand = new AddObjectToSketchWorldRootCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(addCommand);
+            Assert.IsTrue(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "RootSketchObjectGroup");
+
+            DeleteObjectCommand deleteCommand = new DeleteObjectCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(deleteCommand);
+            Invoker.Undo();
+            Assert.IsTrue(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "RootSketchObjectGroup");
+        }
+
+        [Test]
+        public void DeleteObjectRedoTest()
+        {
+            AddObjectToSketchWorldRootCommand addCommand = new AddObjectToSketchWorldRootCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(addCommand);
+            Assert.IsTrue(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "RootSketchObjectGroup");
+
+            DeleteObjectCommand deleteCommand = new DeleteObjectCommand(this.LineSketchObject, this.SketchWorld);
+            Invoker.ExecuteCommand(deleteCommand);
+            Invoker.Undo();
+            Invoker.Redo();
+            Assert.IsFalse(this.LineSketchObject.gameObject.activeInHierarchy);
+            Assert.IsTrue(this.LineSketchObject.transform.IsChildOf(this.SketchWorld.transform));
+            Assert.IsTrue(this.LineSketchObject.transform.parent.name == "Deleted Bin");
+        }
+    }
+}
